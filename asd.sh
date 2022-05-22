5ee0c145e21fcb23d3b05c2bb31c80ce52a96110
Fb185 <fredbobonejr@gmail.com>
2022-05-22 22:58:24 +0100

    added cases

--- asd.sh
+++ asd.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
+directory(){
+  cd && dir=$(cd |fd . '/home/' --type d | fzf -e --tac --reverse --info=inline --border=sharp --color=border:white) && cd -- $dir
+}
 
-cd && file=$(cd |find $HOME/ -type d | fzf -e --tac --reverse --info=inline --border=sharp --color=border:white) && cd -- $file
+file(){
+  cd &&  file=$(cd | rg --files |fzf -e --tac --reverse --info=inline --border=sharp --color=border:white) && vim $file
+}
 
+hiddendir(){ # includes hidden directories
+  cd && dir=$(cd |find $HOME/ -type d | fzf -e --tac --reverse --info=inline --border=sharp --color=border:white) && cd -- $dir
+}
+while getopts ":dfh" option; do
+   case $option in
+      d) 
+         directory
+         exit;;
+      f)
+        file
+        exit;;
+      h)
+        hiddendir
+        exit;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
 
