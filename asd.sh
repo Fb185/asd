2bd8de03c19498469de21b993cc33cc22c6cdcda
Fb185 <fredbobonejr@gmail.com>
2022-10-25 10:30:24 +0100

    Update

--- asd.sh
+++ asd.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 directory(){ # fd excludes hidden directories by default
-  cd && dir=$(cd |fd . '/home/' --type d | fzf -e --tac --reverse --info=inline --border=sharp --color=border:white) && cd -- $dir
+    fd . '/home/' --type d | fzf -e --tac --reverse --info=inline --border=sharp --color=border:white>directory ;cd $(cat directory)
 }
 
 file(){ #rg seems to be faster
-  cd &&  file=$(cd | rg --files |fzf -e --tac --reverse --info=inline --border=sharp --color=border:white) && vim $file
+    file=$(fd . '/home/' --type f | fzf -e --tac --reverse --info=inline --border=sharp --color=border:white>file; xdg-open $(cat file))
 }
 
 hiddendir(){ # includes hidden directories
-  cd && dir=$(cd |find $HOME/ -type d | fzf -e --tac --reverse --info=inline --border=sharp --color=border:white) && cd -- $dir
+  dir=$(find ~/ type f | fzf -e --tac --reverse --info=inline --border=sharp --color=border:white>file; xdg-open $(cat file))
 }
 while getopts ":dfh" option; do
    case $option in
       d) 
          directory
-         exit;;
+         ;;
       f)
         file
-        exit;;
+        ;;
       h)
         hiddendir
-        exit;;
+        ;;
      \?) # Invalid option
          echo "Error: Invalid option"
          exit;;
    esac
 done
 
-directory
